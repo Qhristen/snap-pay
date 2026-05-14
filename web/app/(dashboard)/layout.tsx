7850856193ee9111f3161f09c7076bc22ec2f887
qhristen <odeyxtian@gmail.com>
2026-05-14 17:46:56 +0100

    fix suspense

--- web/app/(dashboard)/layout.tsx
+++ web/app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
-'use client';
 
 import { MobileNav } from '@/components/layout/MobileNav';
+import { Suspense } from 'react';
 
 export default function DashboardLayout({
   children,
@@ -12,7 +12,9 @@ export default function DashboardLayout({
     <div className="min-h-screen bg-background">
       <main className="pb-24 md:pb-8">
         <div className="mx-auto max-w-3xl px-4 py-8 md:px-8">
+          <Suspense>
           {children}
+          </Suspense>
         </div>
       </main>
       <MobileNav />
